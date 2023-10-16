a42cb02dd2145d7fa4857ad8834bc73b7bb850cf
Yohanes Sahrul <Aruna_023@ARUNAs-MacBook-Pro.local>
2023-10-16 16:15:31 +0700

    update meta image 8

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta property="og:image" itemprop="image" content="/images/image-shares.jpg"/>
+        <meta property="og:image:type" content="image/jpg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Gluten:wght@500&family=Grandstander:wght@200&family=Luckiest+Guy&family=Pangolin&display=swap" rel="stylesheet"></link>
